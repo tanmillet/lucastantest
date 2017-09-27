aa888a0f985eb35a79da05af28f7b0b5c8d0e74e
tanmillet <tanmillet@sina.com>
2017-09-27 14:42:14 +0800

    update

---
+++ src/Colors/Green/GreenColor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Colors\Green;
+
+/**
+ * Class GreenColor
+ * User: Terry Lucas
+ * @package Colors\Green
+ */
+class GreenColor
+{
+    /**
+     * @author: Terry Lucas
+     * GreenColor constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * User: Terry Lucas
+     */
+    static public function info()
+    {
+        echo 'printf green';
+    }
+}
---
+++ src/Colors/Red/RedColor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Colors\Red;
+
+/**
+ * Class RedColor
+ * User: Terry Lucas
+ * @package Colors\Red
+ */
+class RedColor
+{
+    /**
+     * @author: Terry Lucas
+     * RedColor constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * User: Terry Lucas
+     */
+    static public function info()
+    {
+        echo 'printf red';
+    }
+}
